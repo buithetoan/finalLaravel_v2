af808217737c519ff5c6078a864a32c41286399c
buithetoan <toan160798@gmail.com>
2020-06-23 13:43:00 +0700

    ToanBT commit merge CRUD slide

---
+++ weaShopOnline/app/Http/Requests/SlideRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SlideRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        if ($this->method()=='PUT'){
+            return [
+                'content' => 'required|max:255|min:5|string',
+                'description' => 'required|max:255|min:10',
+                'display_order' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/',
+                'image' => 'mimes:jpeg,jpg,png',
+                'url' => 'required|max:255|min:1|string',
+            ];
+        }else{
+            return [
+                'content' => 'required|max:255|min:5|string',
+                'description' => 'required|max:255|min:10',
+                'display_order' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/',
+                'image' => 'required|mimes:jpeg,jpg,png',
+                'url' => 'required|max:255|min:1|string',
+            ];
+        }
+    }
+
+    public function messages()
+    {
+        return [
+            'content.required' => 'Please enter content.',
+            'content.string' => 'Do not enter special characters.',
+            'content.max:255' => 'Maximum content length is 255 characters.',
+            'content.min:5' => 'Minimum content length is 5 characters.',
+            'description.required' => 'Please enter description.',
+            'description.max:100' => 'Maximum description length is 255 characters.',
+            'description.min:10' => 'Minimum description length is 10 characters.',
+            'display_order.required' => 'Please enter display order.',
+            'display_order.numeric' =>'Invalid type number.',
+            'image.required' => 'Please select Image.',
+            'image.mimes' => 'Please select file jpg/jpeg/png.',
+            'url.required' => 'Please enter Slug.',
+            'url.string' => 'Do not enter special characters.',
+            'url.max:255' => 'Maximum url length is 255 characters.',
+            'url.min:1' => 'Minimum url length is 1 characters.',
+        ];
+    }
+}
---
+++ weaShopOnline/resources/views/admin/layouts/slides/create.blade.php
@@ -0,0 +1,63 @@
+@extends('admin.shared.main')
+@section('title')
+    Add new slide
+@endsection
+@section('content')
+    <div class="content_yield">
+        {{ Form::open(['url' => 'admin/slide', 'method' => 'post','enctype '=>'multipart/form-data','class' => 'col-md-12 row']) }}
+        <div class="col-md-12 m-auto">
+            <h3 class="mb-5 font-weight-bold">Slide</h3>
+            <div class="col-lg-10 col-md-12 col-sm-12 row">
+                <div class="form-group">
+                    {{ Form::label('Content: ','',['class' => 'font-weight-bold']) }}
+                    {!! Form::text('content', null, [
+                        'class' => 'form-control',
+                        'placeholder'=>"Content"
+                    ])
+                    !!}
+                    <span class="text-danger">{{ $errors->first('content')}}</span>
+                </div>
+                <div class="form-group">
+                    {{ Form::label('Description: ','',['class' => 'font-weight-bold']) }}
+                    {!! Form::textArea('description', null, [
+                        'class' => 'form-control',
+                        'rows' => '3',
+                        'placeholder'=>"Description"
+                    ])
+                    !!}
+                    <span class="text-danger">{{ $errors->first('description')}}</span>
+                </div>
+                <div class="form-group">
+                    {{ Form::label('Url: ','',['class' => 'font-weight-bold']) }}
+                    {!! Form::text('url', null, [
+                        'class' => 'form-control',
+                        'placeholder'=>"Url"
+                    ])
+                    !!}
+                    <span class="text-danger">{{ $errors->first('url')}}</span>
+                </div>
+                <div class="form-group">
+                    {{ Form::label('Display Order: ','',['class' => 'font-weight-bold']) }}
+                    {!! Form::text('display_order', null, [
+                        'class' => 'form-control',
+                        'type' => 'number',
+                        'placeholder'=>"Display Order"
+                    ])
+                    !!}
+                    <span class="text-danger">{{ $errors->first('display_order')}}</span>
+                </div>
+                <div class="form-group">
+                    {{ Form::label('Image: ','',['class' => 'font-weight-bold']) }}
+                    {{ Form::file('image', null, ['class' => 'form-control' ]) }}
+                    <br>
+                    <span class="text-danger">{{ $errors->first('image')}}</span>
+                </div>
+                <div class="form-group text-right">
+                    <a class="btn btn-info mt-3" href="{{ route('slide.index') }}" title="back">Back to list</a>
+                    {{ Form::submit('Save',['class' => 'font-weight-bold text-white btn bg-color-green mt-3']) }}
+                </div>
+            </div>
+        </div>
+        {{ Form::close() }}
+    </div>
+@endsection
