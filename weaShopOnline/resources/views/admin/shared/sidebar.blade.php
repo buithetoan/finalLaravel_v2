11a34df0fea2d335e290867d262fabc868560ec9
buithetoan <toan160798@gmail.com>
2020-06-24 10:05:47 +0700

    ToanBT commit merge dashboard layout

--- weaShopOnline/resources/views/admin/shared/sidebar.blade.php
+++ weaShopOnline/resources/views/admin/shared/sidebar.blade.php
@@ -4,7 +4,7 @@
 		<div class="dash_board_ava">
 			<img src="{{ asset('images/sunflower.png') }}" alt="#">
 		</div>
-		<a class="dash_board_link" href="/admin"> Dashboard </a>
+		<a class="dash_board_link" href="{{ url('/admin') }}"> Dashboard </a>
 	</div>
 	<!-- Manage list -->
 	<li class="list_item">
