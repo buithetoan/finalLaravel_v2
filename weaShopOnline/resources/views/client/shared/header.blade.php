6beb1a79164b2b28488464d8ce791bb0c79a270d
HoiPh <winwin260299@gmail.com>
2020-06-26 18:56:08 +0700

    HoiNP commit remove blog

--- weaShopOnline/resources/views/client/shared/header.blade.php
+++ weaShopOnline/resources/views/client/shared/header.blade.php
@@ -60,7 +60,7 @@
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-menu" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="fa fa-bars"></i>
                 </button>
-                    <a class="navbar-brand" href="index.html"><img src="{{asset('front_assets/images/logo.png')}}" class="logo" alt=""></a>
+                    <a class="navbar-brand" href="{{ url('/home') }}"><img src="{{asset('front_assets/images/logo.png')}}" class="logo" alt=""></a>
                 </div>
                 <!-- End Header Navigation -->
                 <!-- Collect the nav links, forms, and other content for toggling -->
