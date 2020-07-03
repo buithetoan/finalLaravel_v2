ca2c81c3343078302d72f9e420c2ad69e6022059
DinhThanhNghia <dtnghia2510@gmail.com>
2020-07-03 17:12:36 +0700

    NghiaDT commit fix permission

--- weaShopOnline/app/Http/Middleware/CheckLogin.php
+++ weaShopOnline/app/Http/Middleware/CheckLogin.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Auth;
 use App\Providers\RouteServiceProvider;
-
+use App\Models\User;
 class CheckLogin
 {
     /**
@@ -17,13 +17,14 @@ class CheckLogin
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        $roleAdmin = User::find(auth()->guard('admin')->id())->roles()->select('roles.name')->pluck('name')->toArray();
         if(Auth::guard('admin')->check()){
-            if(Auth::guard('admin')->user()->level == 0){
+            if(count($roleAdmin) > 0 && $roleAdmin[0] != 'ROLE_CUSTOMER'){
                 return $next($request);
             }else{
-                return abort(401); 
+                return abort(401);
             }
-        }else{  
+        }else{
             return redirect('admin/login');
         }
     }
