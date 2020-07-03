acc936d844eb922664a5f9f9d0a869ef14e129f5
buithetoan <toan160798@gmail.com>
2020-07-03 10:18:22 +0700

    ToanBT commit merge fix layout role

--- weaShopOnline/resources/views/admin/shared/main.blade.php
+++ weaShopOnline/resources/views/admin/shared/main.blade.php
@@ -35,7 +35,7 @@
 			@include('admin.shared.header')
 			<!-- End NavBar -->
 			<!-- Content Area -->
-			<div class="container-fluid">
+			<div class="container">
 				<!-- Yield content -->
 				@yield('content')
 				<!-- End Yield content -->
